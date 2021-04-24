f07699036c29670ab11bbc437b41f43c76284cc4
Bobo <71311544+SilentsReplacement@users.noreply.github.com>
2021-04-24 22:24:01 +0500

    Humanoid RagDoll State Check

--- src/Client/Client.client.lua
+++ src/Client/Client.client.lua
@@ -8,4 +8,9 @@ humanoid.StateChanged:Connect(function(oldState, newState)
 	if newState == Enum.HumanoidStateType.Landed and math.abs(primaryPart.AssemblyLinearVelocity.Y) > humanoid.JumpPower + humanoid.JumpPower / 2 then
 		primaryPart.AssemblyLinearVelocity *= Vector3.new(1, 0, 1)
 	end
+	
+	-- Rag doll check:
+	if newState == Enum.HumanoidStateType.Ragdoll then
+		primaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
+	end
 end)
