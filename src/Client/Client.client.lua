bd915edb80cd24f502b2b5ad72d71b9a73c6ff62
Bobo <71311544+SilentsReplacement@users.noreply.github.com>
2021-04-29 08:25:44 +0500

    Update Client.client.lua

--- src/Client/Client.client.lua
+++ src/Client/Client.client.lua
@@ -1,14 +1,12 @@
 local character = script.Parent
-local humanoid = character:WaitForChild("Humanoid")
+local humanoid = character:FindFirstChildWhichIsA("Humanoid") or character:WaitForChild("Humanoid", 15)
 local primaryPart = character.PrimaryPart or character:WaitForChild("HumanoidRootPart")
-
--- Stop bouncing when jumping from a high altitude to prevent false positive:
 
 humanoid.StateChanged:Connect(function(oldState, newState)
 	if newState == Enum.HumanoidStateType.Landed and math.abs(primaryPart.AssemblyLinearVelocity.Y) > humanoid.JumpPower + humanoid.JumpPower / 2 then
 		primaryPart.AssemblyLinearVelocity *= Vector3.new(1, 0, 1)
 	end
-	
+
 	-- Rag doll check:
 	if newState == Enum.HumanoidStateType.Ragdoll then
 		primaryPart.AssemblyLinearVelocity = Vector3.new(0, 0, 0)
