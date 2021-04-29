29a5e41adeb024b5091478e616d8d4a821b4559b
ccuser44 <68124053+ccuser44@users.noreply.github.com>
2021-04-29 16:53:23 +0300

    Update Constants.lua

--- src/Server/BoboFighter/Constants.lua
+++ src/Server/BoboFighter/Constants.lua
@@ -1,5 +1,5 @@
 return {
-    PASSIVE_CHECK_INTERVAL = .2,
-    SMALL_DECIMAL = 1e-5, 
-    INVALID_ARGUMENT_FORMAT = "Bad argument to #%s, expected %s, got %s"
+	PASSIVE_CHECK_INTERVAL = .2,
+	SMALL_DECIMAL = 1e-5, 
+	INVALID_ARGUMENT_FORMAT = "Bad argument to #%s, expected %s, got %s"
 }
